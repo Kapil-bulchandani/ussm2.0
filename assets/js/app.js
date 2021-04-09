109be0ca3a67694bb06fb73eaac5df7ca9de574c
ramakrishna0002 <Rkchowdary002@gmail.com>
2021-04-09 10:14:53 -0500

    Ticket No. 3559 - Implementing JSON format UI Changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -36,4 +36,52 @@ jQuery(document).ready(function() {
        span.onclick = function() {
          modal.style.display = "none";
      }
+      /* fm Life Cycle*/
+     jQuery(document).ready(function(){
+      jQuery.ajax({url: window.location.origin+"/business-standards-api/fm/business-lifecycle", success: function(result){              
+             var jObj = JSON.parse(result);
+             jQuery.each(jObj, function(key, value)
+             {     
+                 jQuery.each(value, function(innerKey, innerValue) {                                       
+                     var tr= '<tr>';
+                     var id = innerValue.ID ? innerValue.ID : innerValue.Identifier;
+                     tr += '<td class="capability_class" valign="top">' + id + '</td>'
+                     tr += '<td class="capability_class">' + innerValue.Activity + '</td>'
+                     tr += '<td class="capability_class" valign="top">' + innerValue.Description + '</td>'
+                     tr += '</tr>';
+                     jQuery("#"+key+" > tbody").append(tr);
+                 });
+             });             
+         }});
 
+         jQuery.ajax({url: window.location.origin+"/business-standards-api/fm/", success: function(result){
+          var jObject=JSON.parse(result);
+          jQuery.each(jObject, function (key, value) {
+         if(key=="Business Capabilities")
+          {
+              jQuery.each(value,function(key,value)
+              {npm
+                  var tr='<tr>';
+                   jQuery.each(value,function(key,value){
+                       tr+='<td class="capability_class">'+value+'</td>'     
+                   });
+                   tr+='</tr>';
+                   jQuery("#fm-capabilities > tbody").append(tr);
+               });
+               jQuery("#fm-capabilities").DataTable({
+                                                   responsive: true,
+                                                   dom: 'Bfrtip',
+                                                   buttons: [
+                                                       'copyHtml5',
+                                                       'excelHtml5',
+                                                       'csvHtml5',
+                                                       'pdfHtml5',
+                                                       'pageLength'
+                                                   ]
+                                               } );
+          }               
+          });
+       }});
+     });
+
+     
